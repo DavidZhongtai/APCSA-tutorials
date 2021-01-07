0b10e9905e8874d7e13e85d2f1f05c0175c23b12
David An <davidzhongtaian@gmail.com>
2021-01-07 11:12:29 -0500

    Update palindromcehcker2.java

--- src/palindromcehcker2.java
+++ src/palindromcehcker2.java
@@ -1,3 +1,47 @@
+public class palindromcehcker2 {
+
+    public static void main(String[] args) {
+
+        System.out.println(countPalindromes("Help I have racecar"));
+
+    }
+
+    public static int countPalindromes(String sentence){
+        //stopping condition
+        String palindrome = sentence + "";
+
+        int palindromecounter = 0;
+        String wordCheck = "";
+
+        for(int i = 0; i < palindrome.length(); i++){
+            char ch = palindrome.charAt(i);
+
+            //stopping
+            if(ch!= ' '){
+                wordCheck = wordCheck + ch;
+            } else {
+                if(isPalindrome(wordCheck))
+                    palindromecounter++;
+                wordCheck= "";
+            }
+            //extract words
+        }
+        return palindromecounter;
+    }
+    public static boolean isPalindrome(String text){
+        if(text.equals(reverse(text))){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    public static String reverse(String text){
+
+        String reverseString = ""; //declare return var
+
+        for(int indexReverse = text.length() - 1; indexReverse >=0; indexReverse--){
+            //decrement as we are workign backwards
             reverseString = reverseString + text.charAt(indexReverse); //add it on in end
         }
         return reverseString;
